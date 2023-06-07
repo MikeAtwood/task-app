c1619e5bd0c618881f1a5f97fb11dd5ca951ee28
Deadpool1 <atwood169@yahoo.com>
2023-06-07 16:35:50 -0500

    finished overview

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -7,21 +7,56 @@ class AddTask extends Component {
         this.state = {
             newTask: ''
         }
-        
+        this.handleInputChange = this.handleInputChange.bind(this)
+        this.handleAddTask = this.handleAddTask.bind(this)
+    }
+    handleInputChange = (e) => {
+        this.setState({ newTask: e.target.value })
+    }
+
+    handleAddTask = () => {
+        const { newTask } = this.state;
+        this.props.onAddTask(newTask)
+        this.setState({ newTask: '' })
+    }
+    render() {
+        const { newTask } = this.state
+        return (
+            <div>
+                <input type='text' value={newTask} onChange={this.handleInputChange} />
+                <button onClick={this.handleAddTask}>Add Task</button>
+            </div>
+        )
     }
 }
 
 // Main component that displays content
 class Overview extends Component {
-    
-    
+    constructor(props) {
+        super(props)
+        this.state = {
+            tasks: []
+        };
+        this.handleAddTask = this.handleAddTask.bind(this)
+    }
+    handleAddTask(newTask) {
+        this.setState(prevState => ({
+            tasks: [...prevState.tasks, newTask]
+        }))
+    }
+
     render() {
+        const { tasks } = this.state;
         return (
-                <div>
-                    <h1>Tasks To Do</h1>
-                    <input type='text' placeholder='New Task'></input>
-                    <button>Add+</button>
-                </div>
+            <div>
+                <h1>Tasks To Do</h1>
+                <AddTask onAddTask={this.handleAddTask} />
+                <ul>
+                    {tasks.map((task, index) => (
+                        <li key={index}>{task}</li>
+                    ))}
+                </ul>
+            </div>
         )
     }
 }
